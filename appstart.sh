1484a3663d05d72505100d5637de748f21316ec6
LatrellianChen <34575038+Latrellian@users.noreply.github.com>
2024-11-06 13:38:13 +0800

    Add files via upload

---
+++ appstart.sh
@@ -0,0 +1,7 @@
+result= 'ps -a -o pid,cd |grep "java -jar"|grep -v grep|awk '{ print $1 }'' '
+if [ -n $result ]
+then
+  kill -9 $result
+fi
+nohup java -jar /opt/app.jar > /dev/null 2> /dev/null < /dev/null &
+exit 0
